eb0ed03bc2ed0c53c953ab05751cbf8d5c3303af
martadzielak <marta.dzielak@gmail.com>
2022-01-18 12:07:16 +0100

    fix: fix import

--- src/Overlay/Overlay.jsx
+++ src/Overlay/Overlay.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react/cjs/react.development";
+import { useState } from "react";
 import { Content, OverlayContainer, Toggle } from "./styled";
 import volume from "./volume.png";
 
